861a444008decb6d8962383827e7bd5e6295849d
Vlad <vladik.gutu@gmail.com>
2020-07-13 16:54:48 +0300

    Delete TC01.spec.js

--- TC01.spec.js
+++
@@ -1,9 +0,0 @@
-describe ('Pastebin', function() {
-    it('Create paste', function() {
-        cy.visit('https://pastebin.com')
-        cy.title().should('eq', 'Pastebin.com - #1 paste tool since 2002!')
-        cy.get('textarea[name="paste_code"]').type("Hello World!")
-        cy.get('#submit').contains('Create New Paste').click()
-
-    })
-})  
